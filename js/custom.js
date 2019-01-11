8a5146a2d4d3ecb10db0059deba5cfc8630a2279
Mac <mac@MacBook-Pro-2.fios-router.home>
2019-01-10 21:17:22 -0600

    finished with responsiveness on small devices

--- js/custom.js
+++ js/custom.js
@@ -28,7 +28,21 @@ $(function () {
     mergeFit: true,
     smartSpeed: 700,
     loop: true,
-    autoplayHoverPause: true
+    autoplayHoverPause: true,
+    responsive: {
+      //breakpoint from 0 up
+      0: {
+        items: 1
+      },
+      // breakpoint from 480 up
+      480: {
+        items: 2
+      },
+      // breakpoint from 768 up
+      768: {
+        items: 3
+      }
+    }
   });
 });
 /*======================= 
