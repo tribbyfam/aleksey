d94ee43ea0f4138268473df7d7e6840bb221d7ba
Mac <mac@MacBook-Pro-2.fios-router.home>
2019-01-10 19:28:57 -0600

    styled burger menu for medium devices

--- js/custom.js
+++ js/custom.js
@@ -68,3 +68,10 @@ $(function () {
     }, 1250, "easeInOutExpo");
   });
 });
+
+// Close mobile menu on click //
+$(function () {
+  $(".navbar-collapse ul li a").on("click touch", function () {
+    $(".navbar-toggle").click();
+  });
+});
